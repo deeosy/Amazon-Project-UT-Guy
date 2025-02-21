a2df89fbbedc707afa9df0377cca11ec3547b046
Derrode Walter Cheale <144121696+deeosy@users.noreply.github.com>
2025-02-21 20:30:21 +0000

    added product file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,29 @@
 // step 1 creating and saving the data/ info (this is called a data sturcture - we have sturctured the data with an array and objects)
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87,
-    },
-    priceCents: 1090,   // JS has a problem calculating decimals, in this case money so we will save the money in cents without any decimal
-},{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127,
-    },
-    priceCents: 2095,
-},{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56,
-    },
-    priceCents: 799,
-},]
+// const products = [{
+//     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+//     name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//     rating: {
+//         stars: 4.5,
+//         count: 87,
+//     },
+//     priceCents: 1090,   // JS has a problem calculating decimals, in this case money so we will save the money in cents without any decimal
+// },{
+//     image: 'images/products/intermediate-composite-basketball.jpg',
+//     name: 'Intermediate Size Basketball',
+//     rating: {
+//         stars: 4,
+//         count: 127,
+//     },
+//     priceCents: 2095,
+// },{
+//     image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//     name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+//     rating: {
+//         stars: 4.5,
+//         count: 56,
+//     },
+//     priceCents: 799,
+// }]
 
 //step 3 combining all the html together
 let productsHTML = ''
